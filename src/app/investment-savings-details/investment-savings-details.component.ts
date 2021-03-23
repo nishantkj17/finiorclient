ec61de21a790b9d188d63a14f1cae4ed910766c3
nishantkj17 <nishantkj@gmail.com>
2021-03-23 21:15:45 +0530

    Addec hart on dashboard for reporting

--- src/app/investment-savings-details/investment-savings-details.component.ts
+++ src/app/investment-savings-details/investment-savings-details.component.ts
@@ -10,6 +10,8 @@ import { PrintPDF } from '../common/print-pdf';
 })
 export class InvestmentSavingsDetailsComponent implements OnInit {
   _router: string;
+  showReports:boolean=false;
+  
   constructor(private financialService: financialsService, private router: Router, private printService:PrintPDF) {
 
   }
@@ -17,9 +19,18 @@ export class InvestmentSavingsDetailsComponent implements OnInit {
   ngOnInit(): void {
 
   }
+  public wait(ms){
+    var start = new Date().getTime();
+    var end = start;
+    while(end < start + ms) {
+      end = new Date().getTime();
+   }
+ }
   public printDashBoard()  
   { 
+    //this.showReports=true;
     var data = document.getElementById('FinancialDashboard');  
     this.printService.captureScreen(data, 'FinancialDashboard.pdf');
+    //this.showReports=false;
   }
 }
