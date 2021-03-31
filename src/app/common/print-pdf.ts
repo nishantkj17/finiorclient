997f13b41a2d0026fbe84a83d030fdb8ddbba871
nishantkj17 <nishantkj@gmail.com>
2021-03-31 15:24:53 +0530

    CLosed loan and print page size increase

--- src/app/common/print-pdf.ts
+++ src/app/common/print-pdf.ts
@@ -18,7 +18,8 @@ export class PrintPDF {
         var heightLeft = imgHeight;  
     
         const contentDataURL = canvas.toDataURL('image/png')  
-        let pdf = new jspdf.jsPDF('p', 'mm', 'a4'); // A4 size page of PDF  
+        let pdf = new jspdf.jsPDF('p', 'mm', [210, 350]); // A4 size page of PDF  
+        
         var position = 0;  
         pdf.addImage(contentDataURL, 'PNG', 0, position, imgWidth, imgHeight)  
         pdf.save(filename); // Generated PDF   
